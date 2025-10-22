fb0664aa2a7d0dfdcc36c8ca5d46bbcdad69eb01
DangKhoi <pdkhoi2004@gmail.com>
2025-10-22 13:23:59 +0700

    fix lỗi Trang cho phép đăng ký mật khẩu với toàn khoảng trắng

--- server/src/controllers/auth.schemas.ts
+++ server/src/controllers/auth.schemas.ts
@@ -2,7 +2,15 @@ import { z } from "zod";
 
 export const emailSchema = z.string().email().min(1).max(255);
 
-const passwordSchema = z.string().min(6).max(255);
+//const passwordSchema = z.string().min(6).max(255);
+
+export const passwordSchema = z
+  .string()
+  .min(6, "Password must be at least 6 characters")
+  .max(255)
+  .refine((val) => val.trim().length > 0, {
+    message: "Password cannot be empty or only whitespace",
+  });
 
 export const loginSchema = z.object({
   email: emailSchema,
